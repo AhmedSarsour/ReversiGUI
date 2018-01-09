2fdf397bd639433b103b341b7d93d52c4876dd9e
eliad1998 <eliad98@gmail.com>
2018-01-09 19:00:49 +0200

    Changing xml

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,25 +3,21 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("ReversiGame.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+    public void start(Stage primaryStage) throws Exception {
+        try {
+            HBox root = (HBox)FXMLLoader.load(getClass().getResource("ReversiGame.fxml"));
 
-    /**
-     *
-     * @param args
-     */
+            Scene scene = new Scene(root,520,400);
+            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            primaryStage.setTitle("Maze game"); primaryStage.setScene(scene); primaryStage.show();
+        }
+    catch(Exception e) {e.printStackTrace(); } }
     public static void main(String[] args) {
-        //Method inside application class whenever we start our main java program it will go into the applicaiton and
-        // set the program as javaFX application.
-        launch(args);
-    }
+        launch(args); }
 }
---
+++ src/sample/ReversiController.java
@@ -0,0 +1,22 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.HBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by eliad1998 on 09/01/2018.
+ */
+public class ReversiController implements Initializable {
+    @FXML
+    private HBox root;
+    @Override public void initialize(URL location, ResourceBundle resources) {
+    ReversiBoard mazeBoard = new ReversiBoard(8,8);
+    mazeBoard.setPrefWidth(400);
+    mazeBoard.setPrefHeight(400);
+    root.getChildren().add(0, mazeBoard);
+    mazeBoard.draw(); }
+}
